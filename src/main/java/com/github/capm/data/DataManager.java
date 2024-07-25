9fa34590492297f45a96db18888e36e364b347fc
Gavin Mercer <574285+gmercer@users.noreply.github.com>
2024-07-26 07:47:55 +0800

    DataManager needs to be marked as Repoistory for auto-wiring

--- src/main/java/com/github/capm/data/DataManager.java
+++ src/main/java/com/github/capm/data/DataManager.java
@@ -1,12 +1,15 @@
 package com.github.capm.data;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Repository
 public class DataManager {
     NamedParameterJdbcTemplate template;
 
